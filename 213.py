5f3a11367dae088814c1d4130d4bc5b07f29b9c1
popov-vitaliy <vitamax555@mail.ru>
2021-01-25 21:46:57 +0300

    proba

--- 213.py
+++ 213.py
@@ -1,1 +1,5 @@
-print("Hi my Love")
+print('Hi, %s. %name')
+
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
